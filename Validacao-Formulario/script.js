04459e8fe7d35a3c6d07c81ac4b74fc645d66546
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2025-09-26 19:35:27 -0400

    26-09-2025_19-35-27-Cassiano

---
+++ Validacao-Formulario/script.js
@@ -0,0 +1,6 @@
+const mapa = {"2025.1906.062-7": "Kimberlly Lauany Stachelski canhete",
+    "2022.1904.019-0": "Aron Petta Sarabia",
+    "2022.1904.042-6": "Cassiano Carvalho de Souza"}
+
+
+console.log(mapa["2022.1904.019-0"])
