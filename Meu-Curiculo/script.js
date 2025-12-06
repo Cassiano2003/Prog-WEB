214d952e0e851de181a40474f46571a31c7acf8c
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2025-12-06 09:37:34 -0400

    06-12-2025_09-37-34-Cassiano

---
+++ Meu-Curiculo/script.js
@@ -0,0 +1,13 @@
+document.getElementById("toggle").onclick = () => {
+    document.body.classList.toggle("dark");
+};
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+    var button = document.getElementById("Dark");
+    if (document.body.classList.contains("dark")) {
+        button.textContent = "☀️ Light Mode";
+    } else {
+        button.textContent = "🌙 Dark Mode";
+    }
+}
