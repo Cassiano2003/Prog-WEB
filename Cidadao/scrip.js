939ec9e470cc5871c1afd8923898218db82449f5
Cassiano2003 <cassianocarvalhodesouza@gmail.com>
2026-01-01 11:19:27 -0400

    01-01-2026_11-19-27-Cassiano

--- Cidadao/scrip.js
+++
@@ -1,8 +0,0 @@
-function pegarNome() {
-    var nome = document.getElementById("nome").value;
-    const div = document.getElementById("coloca");
-    div.innerHTML = ""; // Limpa o conteúdo anterior
-    var h1 = document.createElement("h1");
-    h1.textContent = nome + " GAY";
-    div.appendChild(h1);
-}
